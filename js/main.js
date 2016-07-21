7a7a73c014ff249b54516d64828f4999159e27ea
Faisal Hossain <fhossain@ael.net>
2016-07-21 20:38:43 +0600

    Demo Website of DCL Initial Commit

---
+++ js/main.js
@@ -0,0 +1,131 @@
+jQuery(function($) {
+
+	//Preloader
+	var preloader = $('.preloader');
+	$(window).load(function(){
+		preloader.remove();
+	});
+
+	//#main-slider
+	var slideHeight = $(window).height();
+	$('#home-slider .item').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('#home-slider .item').css('height',slideHeight);
+	});
+	
+	//Scroll Menu
+	$(window).on('scroll', function(){
+		if( $(window).scrollTop()>slideHeight ){
+			$('.main-nav').addClass('navbar-fixed-top');
+		} else {
+			$('.main-nav').removeClass('navbar-fixed-top');
+		}
+	});
+	
+	// Navigation Scroll
+	 $(window).scroll(function(event) {
+	 	 Scroll();
+	 });
+
+	 $('.navbar-collapse ul li a').on('click', function() {  
+	 	$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+	 	return false;
+	 });
+
+	// User define function
+	 function Scroll() {
+	 	var contentTop      =   [];
+	 	var contentBottom   =   [];
+	 	var winTop      =   $(window).scrollTop();
+	 	var rangeTop    =   200;
+	 	var rangeBottom =   500;
+	 	$('.navbar-collapse').find('.scroll a').each(function(){
+	 		contentTop.push( $( $(this).attr('href') ).offset());
+	 		contentBottom.push( $( $(this).attr('href') ).offset() + $( $(this).attr('href') ).height() );
+	 	})
+	 	$.each( contentTop, function(i){
+	 		if ( winTop > contentTop[i] - rangeTop ){
+	 			$('.navbar-collapse li.scroll')
+	 			.removeClass('active')
+ 			.eq(i).addClass('active');			
+	 		}
+	 	})
+	 };
+
+	$('#tohash').on('click', function(){
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+		return false;
+	});
+	
+	//Initiat WOW JS
+	new WOW().init();
+	//smoothScroll
+	smoothScroll.init();
+	
+	// Progress Bar
+	$('#about-us').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+		if (visible) {
+			$.each($('div.progress-bar'),function(){
+				$(this).css('width', $(this).attr('aria-valuetransitiongoal')+'%');
+			});
+			$(this).unbind('inview');
+		}
+	});
+
+	//Countdown
+	$('#features').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+		if (visible) {
+			$(this).find('.timer').each(function () {
+				var $this = $(this);
+				$({ Counter: 0 }).animate({ Counter: $this.text() }, {
+					duration: 2000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.ceil(this.Counter));
+					}
+				});
+			});
+			$(this).unbind('inview');
+		}
+	});
+
+
+	//Pretty Photo
+	 $("a[data-gallery^='prettyPhoto']").prettyPhoto({
+	  social_tools: false
+	 });
+
+	// portfolio filter
+		//  $(window).load(function(){'use strict',
+		//  	$portfolio_selectors = $('.portfolio-filter >li>a');
+		//  	if($portfolio_selectors!='undefined'){
+		//  		$portfolio = $('.portfolio-items');
+		//  		$portfolio.isotope({
+		//  			itemSelector : '.col-sm-3',
+		//  			layoutMode : 'fitRows'
+		//  		});
+				
+		//  		$portfolio_selectors.on('click', function(){
+		//  			$portfolio_selectors.removeClass('active');
+		//  			$(this).addClass('active');
+		//  			var selector = $(this).attr('data-filter');
+		//  			$portfolio.isotope({ filter: selector });
+		//  			return false;
+		//  		});
+		//  	}
+		//  });
+
+		// jQuery Owl Carousel
+		$(".partner-list").owlCarousel({
+			pagination : false,
+			navigation : true,
+			navigationText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+		});
+
+		
+	
+});
+
+
+
